a3e0f5e045a1f042c600524240ac3b2c7a2f9913
muhamad-bari <barsid589@gmail.com>
2026-04-18 06:07:22 +0700

    update gitignore

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitac2d307f4ed514763a8119f3523066bb::getLoader();
+return ComposerAutoloaderInitd78bb64c7424c7bb21abd9a88ac7de05::getLoader();
